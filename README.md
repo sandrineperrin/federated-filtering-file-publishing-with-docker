0807d4c74e916058b8778a7183955891a590435b
bryan.brancotte@github.com <bryan.brancotte@gmail.com>
2016-07-12 17:19:18 +0200

    import scripts and howto

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # federated-filtering-proxy-with-docker
+
+export ALLOWED_EMAIL_COMMA_SEPARATED_VALUES="john.doe@no.where, bowie@space.oddity"
+
+./startFilteringProxy.sh
---
+++ startFilteringProxy.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+DOCKER_IMAGE_OWNER=cyclone
+DOCKER_IMAGE_NAME=federated-filtering-proxy
+FQDN=$(       hostname -I | sed 's/ /\n/g' | grep -v 172.17 | head -n 1)
+TARGET_FQDN=$(hostname -I | sed 's/ /\n/g' | grep    172.17 | head -n 1)
+TARGET_PORT=${TARGET_PORT:-8080}
+TARGET_PATH=${TARGET_PATH:-Insyght/}
+
+sudo mkdir -p /var/log/httpd-federated-filtering-proxy
+
+sudo service docker start
+
+#ALLOWED_EMAIL_COMMA_SEPARATED_VALUES=${ALLOWED_EMAIL_COMMA_SEPARATED_VALUES:-john.doe@no.where, bowie@space.oddity}
+
+if [ "$ALLOWED_EMAIL_COMMA_SEPARATED_VALUES" == "" ];
+	echo "env var \$ALLOWED_EMAIL_COMMA_SEPARATED_VALUES must contains edugain email of allowed user"
+	exit 1
+fi
+
+cat "cyclone: $ALLOWED_EMAIL_COMMA_SEPARATED_VALUES" > apache_groups
+
+echo "to open $TARGET_PORT:\niptables -I INPUT 1 -p tcp -i docker0 -m tcp --dport $TARGET_PORT -j ACCEPT"
+
+docker rm -f federated-filtering-proxy
+docker build -t ${DOCKER_IMAGE_OWNER}/${DOCKER_IMAGE_NAME}  \
+	-f Dockerfile . &&  \
+docker run -it -p 80:80 \
+	-e FQDN=${FQDN}  \
+	-e TARGET_FQDN=${TARGET_FQDN}  \
+	-e TARGET_PORT=${TARGET_PORT} \
+	-e TARGET_PATH=${TARGET_PATH} \
+	-v /var/log/httpd-federated-filtering-proxy:/var/log/httpd   \
+	--name ${DOCKER_IMAGE_NAME}  \
+	${DOCKER_IMAGE_OWNER}/${DOCKER_IMAGE_NAME}
+
