114dd889dec8074644de9b761347c8595ae4a910
bryan.brancotte@github.com <bryan.brancotte@gmail.com>
2016-09-14 11:29:57 +0200

    Can use an image based on debian in place of centos7, to do so 'export DOCKERFILE=Dockerfile.debian' before running startFilteringProxy.sh

--- startFilteringProxy.sh
+++ startFilteringProxy.sh
@@ -5,12 +5,28 @@ FQDN=${FQDN:-$(              hostname -I | sed 's/ /\n/g' | grep -v 172.17 | hea
 TARGET_FQDN=${TARGET_FQDN:-$(hostname -I | sed 's/ /\n/g' | grep    172.17 | head -n 1)}
 TARGET_PORT=${TARGET_PORT:-8080}
 TARGET_PATH=${TARGET_PATH:-/Insyght/}
-DEAMON_OR_ITERACTIVE=${DEAMON_OR_ITERACTIVE:-d}
+if [ -z "$1" ]
+then
+	DEFAULT_DEAMON_OR_ITERACTIVE=d
+else
+	DEFAULT_DEAMON_OR_ITERACTIVE=it
+fi
+DEAMON_OR_ITERACTIVE=${DEAMON_OR_ITERACTIVE:-$DEFAULT_DEAMON_OR_ITERACTIVE}
 SUDO_CMD=${SUDO_CMD:-sudo}
+DOCKERFILE=${DOCKERFILE:-Dockerfile}
+LOG_DIR=${LOG_DIR:-/var/log/httpd-federated-filtering-proxy}
 
-$SUDO_CMD mkdir -p /var/log/httpd-federated-filtering-proxy
+if [ ! -d $LOG_DIR ]
+then
+	echo "LOG_DIR(=$LOG_DIR) is missing, creating it"
+	$SUDO_CMD mkdir -p $LOG_DIR
+fi
 
-$SUDO_CMD service docker start
+if [ "$(docker ps 1>/dev/null 2>/dev/null ; echo $?)" != "0" ]
+then
+	echo "Docker seems to not be running"
+	$SUDO_CMD service docker start
+fi
 
 if [ "$ALLOWED_EMAIL_SPACE_SEPARATED_VALUES" != "" ]
 then
@@ -45,15 +61,16 @@ echo "to open $TARGET_PORT:\niptables -I INPUT 1 -p tcp -i docker0 -m tcp --dpor
 
 docker rm -f federated-filtering-proxy
 docker build -t ${DOCKER_IMAGE_OWNER}/${DOCKER_IMAGE_NAME}  \
-	-f Dockerfile . &&  \
+	-f ${DOCKERFILE} . &&  \
 docker run -${DEAMON_OR_ITERACTIVE} -p 80:80 \
 	-e FQDN=${FQDN}  \
 	-e TARGET_FQDN=${TARGET_FQDN}  \
 	-e TARGET_PORT=${TARGET_PORT} \
 	-e TARGET_PATH=${TARGET_PATH} \
-	-v /var/log/httpd-federated-filtering-proxy:/var/log/httpd \
+	-v ${LOG_DIR}:/var/log/httpd \
 	-v $PWD/proxy.conf:/etc/httpd/conf.d/proxy.conf:ro \
+	-v $PWD/proxy.conf:/etc/apache2/conf-enabled/proxy.conf:ro \
 	-v $PWD/apache_groups:/etc/httpd/apache_groups:ro \
 	--name ${DOCKER_IMAGE_NAME}  \
-	${DOCKER_IMAGE_OWNER}/${DOCKER_IMAGE_NAME}
+	${DOCKER_IMAGE_OWNER}/${DOCKER_IMAGE_NAME} $1
 
